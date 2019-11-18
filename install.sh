bf166487fcf04f8f1304e1a6defbbcd73593a5b0
Neeraj Jaiswal <neerajj85@gmail.com>
2019-11-18 09:33:11 +0530

    Update script for docker installation

--- install.sh
+++ install.sh
@@ -1,6 +1,31 @@
 #! /bin/bash
 
-apt-get update
+DOCKER_COMPOSE_VERSION=1.24.1
 
+apt-get update
+apt-get upgrade
+
+install_docker() {
+  apt-get remove docker docker-engine docker.io containerd runc
+  apt autoremove
+  apt-get install \
+    apt-transport-https ca-certificates curl \
+    gnupg-agent software-properties-common
+
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+  add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+  apt-get install docker-ce docker-ce-cli containerd.io
+}
+
+install_compose() {
+  curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+}
+
+install_docker
+install_compose
 apt-get install watchexec.deb
-apt-get install docker
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker-compose up -d --remove-orphans --force-recreate --build
+# docker-compose up -d --remove-orphans --force-recreate --build
+docker-compose up -d --remove-orphans --build
