2a80c78a1c4baabc4ee88b86c63fa00dc506c85f
Neeraj Jaiswal <neerajj85@gmail.com>
2020-01-31 17:02:00 +0530

    Add fish and -y option when installing packages

--- install.sh
+++ install.sh
@@ -3,21 +3,19 @@
 DOCKER_COMPOSE_VERSION=1.24.1
 
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 
 install_docker() {
-  apt-get remove docker docker-engine docker.io containerd runc
-  apt autoremove
+  apt-get remove docker docker-engine docker.io containerd runc -y
   apt-get install \
     apt-transport-https ca-certificates curl \
-    gnupg-agent software-properties-common
+    gnupg-agent software-properties-common -y
 
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-  add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-  apt-get install docker-ce docker-ce-cli containerd.io
+  apt-get install docker-ce docker-ce-cli containerd.io -y
 }
 
 install_compose() {
@@ -26,6 +24,8 @@ install_compose() {
   ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 }
 
+apt-get install fish -y
 install_docker
 install_compose
-apt-get install watchexec.deb
+apt-get install ./watchexec.deb -y
+apt autoremove
